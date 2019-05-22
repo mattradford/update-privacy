705b0653e2b35fe6a518ff4a16b01c7ca862eae1
Matt Radford <webform@mattrad.co.uk>
2019-05-22 22:20:10 +0100

    Update version PSR-2

--- update-privacy.php
+++ update-privacy.php
@@ -1,17 +1,17 @@
 <?php
-/*
-	Plugin Name: Update Privacy
-	Plugin URI: https://wordpress.org/plugins/update-privacy
-	Description: Stop WordPress sending anything but essential data during the update check.
-	Author: Matt Radford
-	Author URI: https://mattrad.uk
-	Version: 1.0.2
-	License: GPLv2 or later
-	License URI: https://www.gnu.org/licenses/gpl-2.0.html
-*/
+/**
+* Plugin Name: Update Privacy
+* Plugin URI: https://wordpress.org/plugins/update-privacy
+* Description: Stop WordPress sending anything but essential data during the update check.
+* Author: Matt Radford
+* Author URI: https://mattrad.uk
+* Version: 1.0.3
+* License: GPLv2 or later
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+**/
 
-function mattrad_update_privacy( $query ) {
-	
+function mattrad_update_privacy($query)
+{
 	unset($query['php']);
 	unset($query['mysql']);
 	unset($query['local_package']);
@@ -19,8 +19,6 @@ function mattrad_update_privacy( $query ) {
 	unset($query['users']);
 	unset($query['multisite_enabled']);
 	unset($query['initial_db_version']);
-
 	return $query;
 }
-
-add_action( 'core_version_check_query_args', 'mattrad_update_privacy' );
+add_action('core_version_check_query_args', 'mattrad_update_privacy');
